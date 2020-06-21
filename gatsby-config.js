8ad82a8ea18fb9ab6583f358dab8b6cd60aa1d10
Ian C <ian@thecommittedbug.io>
2020-06-21 10:28:57 -0400

    additional support

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,13 @@ module.exports = {
       },
     },
     "gatsby-plugin-netlify",
-    `gatsby-plugin-netlify-cms`,
-
+    {
+      resolve: `gatsby-plugin-netlify-identity`,
+      options: {
+        url: `https://the-gluttonous-anorexic.netlify.com` // required!
+      }
+    },
+    "gatsby-plugin-netlify-cms",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
